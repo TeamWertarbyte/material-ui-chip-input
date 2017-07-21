388332456aebf2e602e9f7f225692fb0a52390ef
Yoshiaki Tsutsumi <yoshiaki.tsutsumi@byus.me>
2017-07-21 21:52:41 +0900

    Clear input on Enter key is pressed after keypress event

--- src/ChipInput.js
+++ src/ChipInput.js
@@ -292,10 +292,10 @@ class ChipInput extends React.Component {
   }
 
   handleKeyUp = (event) => {
-    if (this.props.newChipKeyCodes.indexOf(event.keyCode) < 0 && this.state.keyPressed) {
-      this.setState({ inputValue: event.target.value })
-    } else if (!this.state.keyPressed) {
+    if (this.props.newChipKeyCodes.indexOf(event.keyCode) > 0 && this.state.keyPressed) {
       this.clearInput()
+    } else{
+      this.setState({ inputValue: event.target.value })
     }
   }
 
