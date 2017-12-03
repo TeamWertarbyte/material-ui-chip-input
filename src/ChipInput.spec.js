8b3db903e923ae7a69a7df5960a94cb9f71c7fbb
leMaik <an@maik.tk>
2017-12-03 19:48:46 +0100

    Add tests for clearOnBlur.

--- src/ChipInput.spec.js
+++ src/ChipInput.spec.js
@@ -314,3 +314,21 @@ describe('custom chips', () => {
     })
   })
 })
+
+it('clears the input on blur', () => {
+  const tree = mount(
+    <ChipInput value={['a', 'b']} />
+  )
+  tree.find('input').simulate('change', { target: { value: 'foo' } })
+  tree.find('input').simulate('blur')
+  expect(tree.find('input').getDOMNode().value).toBe('')
+})
+
+it('does not clear the input on blur with clearOnBlur set to false', () => {
+  const tree = mount(
+    <ChipInput value={['a', 'b']} clearOnBlur={false} />
+  )
+  tree.find('input').simulate('change', { target: { value: 'foo' } })
+  tree.find('input').simulate('blur')
+  expect(tree.find('input').getDOMNode().value).toBe('foo')
+})
