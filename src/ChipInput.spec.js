40c6efc11a8a2db39488059e0d384f899787611f
Tobias Nendel <tobias.nendel@scubical.com>
2018-03-11 16:05:43 +0100

    fix linting. maybe we should add a pre commit hook here.

--- src/ChipInput.spec.js
+++ src/ChipInput.spec.js
@@ -336,42 +336,40 @@ describe('custom chips', () => {
 })
 
 describe('blurBehavior modes', () => {
-    it('clears the input on blur', () => {
-      const tree = mount(
-        <ChipInput value={['a', 'b']} />
+  it('clears the input on blur', () => {
+    const tree = mount(
+      <ChipInput value={['a', 'b']} />
       )
-      tree.find('input').simulate('change', { target: { value: 'foo' } })
-      tree.find('input').simulate('blur')
-      expect(tree.find('input').getDOMNode().value).toBe('')
+    tree.find('input').simulate('change', { target: { value: 'foo' } })
+    tree.find('input').simulate('blur')
+    expect(tree.find('input').getDOMNode().value).toBe('')
+  })
+
+  it('does not clear the input on blur with blurBehavior set to ignore', () => {
+    const tree = mount(
+      <ChipInput value={['a', 'b']} blurBehavior='ignore' />
+      )
+    tree.find('input').simulate('change', { target: { value: 'foo' } })
+    tree.find('input').simulate('blur')
+    expect(tree.find('input').getDOMNode().value).toBe('foo')
+  })
+
+  it('adds the input on blur with blurBehavior set to add', () => {
+    const handleChange = jest.fn()
+    jest.useFakeTimers()
+    const tree = mount(
+      <ChipInput defaultValue={['a', 'b']} blurBehavior='add' onChange={handleChange} />
+      )
+    tree.find('input').getDOMNode().value = 'blur'
+    tree.find('input').simulate('blur')
+
+    expect(tree.find('input').getDOMNode().value).toBe('')
+    expect(setTimeout).toHaveBeenCalledTimes(1)
+
+    setTimeout(_ => {
+      expect(tree.find('Chip').map((chip) => chip.text())).toEqual(['a', 'b', 'blur'])
     })
 
-    it('does not clear the input on blur with blurBehavior set to ignore', () => {
-      const tree = mount(
-        <ChipInput value={['a', 'b']} blurBehavior="ignore" />
-      )
-      tree.find('input').simulate('change', { target: { value: 'foo' } })
-      tree.find('input').simulate('blur')
-      expect(tree.find('input').getDOMNode().value).toBe('foo')
-    })
-
-    it('adds the input on blur with blurBehavior set to add', () => {
-      const handleChange = jest.fn()
-      jest.useFakeTimers();
-      const tree = mount(
-        <ChipInput defaultValue={['a', 'b']} blurBehavior="add" onChange={handleChange}/>
-      )
-      tree.find('input').getDOMNode().value = 'blur'
-      tree.find('input').simulate('blur')
-
-      expect(tree.find('input').getDOMNode().value).toBe('')
-      expect(setTimeout).toHaveBeenCalledTimes(1);
-
-      setTimeout( _ => {
-        expect(tree.find('Chip').map((chip) => chip.text())).toEqual(['a', 'b', 'blur'])
-      })
-
-      jest.runAllTimers();
-
-
-    })
+    jest.runAllTimers()
+  })
 })
