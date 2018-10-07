b23dc41a6b31e29ae1d3f5824db29782a6c43e7d
Harshit Pant <pantharshit00@gmail.com>
2018-10-07 22:58:55 +0530

    add typings

---
+++ typings/index.d.ts
@@ -0,0 +1,34 @@
+import * as React from 'react';
+
+interface Props {
+  allowDuplicates?: boolean;
+  blurBehavior?: 'clear' | 'add' | 'ignore';
+  chipRenderer?: (...args: any[]) => any;
+  clearInputValueOnChange?: boolean;
+  dataSource?: any[];
+  dataSourceConfig?: {
+    text: string;
+    value: string;
+  };
+  defaultValue?: any[];
+  disabled?: boolean;
+  FormHelperTextProps?: object;
+  fullWidth?: boolean;
+  fullWidthInput?: boolean;
+  helperText?: React.ReactNode;
+  InputLabelProps?: object;
+  InputProps?: object;
+  inputRef?: (...args: any[]) => any;
+  label?: React.ReactNode;
+  newChipKeyCodes?: number[];
+  onAdd?: (...args: any[]) => any;
+  onBeforeAdd?: (...args: any[]) => any;
+  onChange?: (...args: any[]) => any;
+  onDelete?: (...args: any[]) => any;
+  onUpdateInput?: (...args: any[]) => any;
+  placeholder?: string;
+  value?: any[];
+}
+
+declare const ChipInput: React.Component<Props>;
+export default ChipInput;
