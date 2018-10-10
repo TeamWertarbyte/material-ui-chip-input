890de052b43097b4913f0186f0be337edaf3bbac
Harshit Pant <pantharshit00@gmail.com>
2018-10-10 12:04:03 +0530

    Add TS definitions (#233)
    * add typings to package.json
    
    * add typings
    
    * add typing on publish
    
    * change component to componentType
    
    * add error to types
    
    * update typings
    
    * change card to any
    
    * fix more types

---
+++ typings/index.d.ts
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { FormHelperTextProps } from '@material-ui/core/FormHelperText'
+import {InputProps} from '@material-ui/core/Input'
+import {InputLabelProps} from '@material-ui/core/InputLabel'
+
+export interface ChipRendererArgs {
+  value: string;
+  text: string;
+  chip: any;
+  isFocused: boolean;
+  isDisabled: boolean;
+  handleClick: React.EventHandler<any>;
+  handleDelete: React.EventHandler<any>;
+  className: string;
+}
+
+export type ChipRenderer = (
+  args: ChipRendererArgs,
+  key: any
+) => React.ReactNode;
+
+export interface Props {
+  allowDuplicates?: boolean;
+  blurBehavior?: 'clear' | 'add' | 'ignore';
+  chipRenderer?: ChipRenderer;
+  clearInputValueOnChange?: boolean;
+  dataSource?: any[];
+  dataSourceConfig?: {
+    text: string;
+    value: string;
+  };
+  defaultValue?: any[];
+  disabled?: boolean;
+  FormHelperTextProps?: FormHelperTextProps;
+  fullWidth?: boolean;
+  fullWidthInput?: boolean;
+  helperText?: React.ReactNode;
+  InputLabelProps?: InputLabelProps;
+  InputProps?: InputProps;
+  inputRef?: (ref: React.Ref<HTMLInputElement>) => any;
+  label?: React.ReactNode;
+  newChipKeyCodes?: number[];
+  onAdd?: (chip: any) => any;
+  onBeforeAdd?: (chip: any) => boolean;
+  onChange?: (chips: any[]) => any;
+  onDelete?: (chip:any, index: number) => any;
+  onUpdateInput?: React.EventHandler<any>
+  placeholder?: string;
+  value?: any[];
+  error?: boolean;
+}
+
+declare const ChipInput: React.ComponentType<Props>;
+export default ChipInput;
