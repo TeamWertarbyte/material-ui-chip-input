c1eb5dd3fd294a5a01e57d6250e7bac114c72982
modeek <admin@myflow.app>
2020-03-09 12:30:45 -0400

    test
    test

--- test/jestsetup.js
+++
@@ -1,8 +0,0 @@
-import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-// React 16 Enzyme adapter
-Enzyme.configure({ adapter: new Adapter() })
-// Fail tests on any warning
-console.error = message => {
-  throw new Error(message)
-}
--- test/shim.js
+++
@@ -1,4 +0,0 @@
-// temporary fix, see https://github.com/facebookincubator/create-react-app/issues/3199 and https://github.com/facebook/jest/issues/4545
-global.requestAnimationFrame = (cb) => {
-  setTimeout(cb, 0)
-}
